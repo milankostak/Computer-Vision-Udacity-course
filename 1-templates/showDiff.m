0db418f19e6cee3fb318aae1e81964c54276bdbb
Milan Košťák <milankostak@seznam.cz>
2018-10-15 20:28:06 +0200

    1 - templates

---
+++ 1-templates/showDiff.m
@@ -0,0 +1,7 @@
+% show location of the found location of a subset in the orignal image
+function showDiff(x, y, subset, original)
+    colormap('gray'), imshow(original);
+    hold on;
+    rectangle('Position', [x, y, size(subset, 2), size(subset, 1)], 'EdgeColor', 'b');
+    hold off;
+endfunction
