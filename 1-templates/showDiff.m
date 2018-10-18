c2f026a050cfd10a2396d40c298bb4f8121ab7df
Milan Košťák <milankostak@seznam.cz>
2018-10-18 15:59:48 +0200

    fixes in formatting

--- 1-templates/showDiff.m
+++ 1-templates/showDiff.m
@@ -1,5 +1,8 @@
 % show location of the found location of a subset in the orignal image
 function showDiff(x, y, subset, original)
+
+    pkg load image;
+
     colormap('gray'), imshow(original);
     hold on;
     rectangle('Position', [x, y, size(subset, 2), size(subset, 1)], 'EdgeColor', 'b');
